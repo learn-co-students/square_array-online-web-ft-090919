1c05e183eb7c8d0613a47ff73e88131079b84011
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-11 18:56:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,5 @@
 def square_array(array)
   squared = []
-  count = 0
-  while count < array.length do
-    squared << (array[count] ** 2)
-    count = count + 1
-    array.each |num| do 
-      array ** 2
-    end
-  end
+  array.each { |element| squared << element ** 2 }
   squared
 end
