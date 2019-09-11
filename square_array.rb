6b0a2d532f7b8f63e63677db4f6833184f64145d
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 17:21:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  sq_array = []
+  array.each { | num | sq_array.push(num ** 2) }
+  sq_array
 end
+
+# def square_array(array)
+#   array.collect do | num |
+#     num ** 2 
+#   end
+# end
